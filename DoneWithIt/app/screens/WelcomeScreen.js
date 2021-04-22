6598e73e5c874ba07e0dbbbbd4534f2cd0246ed7
GeethuAdarsh <geethukrishna1011@gmail.com>
2021-04-22 12:51:58 +0530

    App folder of the project

---
+++ DoneWithIt/app/screens/WelcomeScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Image, ImageBackground, StyleSheet, View } from 'react-native';
+
+function WelcomeScreen(props) {
+    return (
+        <ImageBackground
+        style = {styles.background}
+        source = {require("../assets/background.jpg")}>
+        <Image style={styles.logo} source = {require("../assets/logo.png")} />
+        <View style={styles.loginButton}></View>
+        <View style={styles.registerButton}></View>
+        </ImageBackground>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    background : {
+        flex : 1,
+        justifyContent : "flex-end"
+    },
+    loginButton : {
+        width : '100%',
+        height : 70,
+        backgroundColor : '#fc5c65'
+    },
+    registerButton : {
+        width : '100%',
+        height : 70,
+        backgroundColor : '#4ecdc4'
+    }
+})
+
+export default WelcomeScreen;
