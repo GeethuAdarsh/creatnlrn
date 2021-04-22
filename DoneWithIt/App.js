9f31db10ab2d0b49db85da759357f2e8a77812d9
GeethuAdarsh <geethukrishna1011@gmail.com>
2021-04-22 12:49:42 +0530

    next files of the react project

---
+++ DoneWithIt/App.js
@@ -0,0 +1,11 @@
+//import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, StyleSheet} from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+export default function App() {
+  return <WelcomeScreen/>
+}
+
+const styles = StyleSheet.create({
+  
+});
